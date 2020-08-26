26ffd130888a2c6956362951f75c4eda40d8d4cf
Andrew Thomas <athomik79@gmail.com>
2020-08-26 18:28:25 -0400

    Created Repos & RepoItem components, added to user profile

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   };
 
   render() {
@@ -31,7 +35,7 @@ export class User extends Component {
       hireable,
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -64,7 +68,12 @@ export class User extends Component {
                 <p>{bio}</p>
               </Fragment>
             )}
-            <a href={html_url} className='btn btn-dark my-1'>
+            <a
+              href={html_url}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='btn btn-dark my-1'
+            >
               Visit Github Profile
             </a>
             <ul>
@@ -88,13 +97,14 @@ export class User extends Component {
                 {blog && (
                   <Fragment>
                     <strong>Website: </strong>
-                    <a
+                    {blog}
+                    {/* <a
                       href={`http://${blog}`}
                       target='_blank'
                       rel='noopener noreferrer'
                     >
                       {blog}
-                    </a>
+                    </a> */}
                   </Fragment>
                 )}
               </li>
@@ -107,6 +117,7 @@ export class User extends Component {
           <div className='badge badge-light'>Public Repos: {public_repos}</div>
           <div className='badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
